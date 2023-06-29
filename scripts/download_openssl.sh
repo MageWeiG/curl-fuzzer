e930c7f3cb8dd264b7707cf85cdae64c0ee09ab7
MageWeiG <34268715+MageWeiG@users.noreply.github.com>
2023-06-29 11:00:18 +0800

    Update download_openssl.sh

--- scripts/download_openssl.sh
+++ scripts/download_openssl.sh
@@ -4,4 +4,10 @@
 set -ex
 
 # Clone the repository to the specified directory.
-git clone --depth 1 --branch openssl-3.0.7 https://github.com/openssl/openssl $1
+# git clone --depth 1 --branch openssl-3.0.7 https://github.com/openssl/openssl $1
+
+wget https://github.com/openssl/openssl/releases/download/openssl-3.0.7/openssl-3.0.7.tar.gz -O /tmp/src.tar.gz
+tar -xvf /tmp/src.tar.gz --directory /src
+ 
+# Move the directory into the correct place
+mv -v /src/openssl-3.0.7 "$1"
